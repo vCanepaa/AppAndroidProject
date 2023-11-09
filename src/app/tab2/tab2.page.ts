1ec4d6b80c27dcd08a52e4adfd2134813f913693
mateus sena <mtsnascmiento@hotmail.com>
2023-11-09 02:38:04 -0300

    primeiro commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Refeicao } from './Refeicao';
 import { formatNumber } from '@angular/common';
+import{Storage} from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-tab2',
@@ -17,28 +18,51 @@ export class Tab2Page {
   refeicoes: Refeicao[] =[];
 
 
-  constructor() {}
+  constructor(public storage:Storage) {
+    this.init();
+  }
+  async init() {
+    await this.storage.create();
+    this.getRefeicao();
+  }
+  async limparBanco() {
+    await this.storage.clear(); 
 
-  async adicionaRefeicao(){
-    if(this.nome && this.hora && this.desc  && this.valorCal){
-    const horaTemp = this.hora.split(":");
-    this.horario.setHours(parseInt(horaTemp[0]));
-    this.horario.setMinutes(parseInt(horaTemp[1]));
+    
+    
+    this.refeicoes = []; 
+  }
+    
+  
+  
 
+  async getRefeicao() {
+    this.refeicoes = await this.storage.get('refeicoes') || [];
+  }
 
-    const novaRefeicao = new Refeicao(this.horario,this.nome,this.valorCal,this.desc);
-    this.refeicoes.push(novaRefeicao);
+  async adicionaRefeicao() {
+    if (this.nome && this.hora && this.desc && this.valorCal) {
+      const horaTemp = this.hora.split(":");
+      this.horario.setHours(parseInt(horaTemp[0]));
+      this.horario.setMinutes(parseInt(horaTemp[1]));
+  
+      const novaRefeicao = new Refeicao(this.horario, this.nome, this.valorCal, this.desc);
+  
+      this.refeicoes.push(novaRefeicao);
+      await this.storage.set('refeicoes', this.refeicoes); 
+
+      this.getRefeicao();
     }
+  
     this.nome = '';
     this.desc = '';
     this.valorCal = 0;
-
-
-
+  
     this.toggleForm();
   }
   async toggleForm(){
     this.mostrarForm = !this.mostrarForm;
   }
+}
 
-} 
+ 
